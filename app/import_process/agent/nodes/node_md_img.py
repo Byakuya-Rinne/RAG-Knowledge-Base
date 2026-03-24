06964ffa612f4019329d524153188fe9a16f9bfe
Byakuya-Rinne <2353845636@qq.com>
2026-03-24 18:54:35 +0800

    MD图片处理注释

--- app/import_process/agent/nodes/node_md_img.py
+++ app/import_process/agent/nodes/node_md_img.py
@@ -3,16 +3,29 @@ from app.import_process.agent.state import ImportGraphState
 from app.core.logger import logger
 
 class NodeMdImg(NodeBase):
-    """
-    节点: 图片处理 (node_md_img)
-    1. 扫描 Markdown 中的图片链接。
-    2. 将图片上传到 MinIO 对象存储。
-    3. (可选) 调用多模态模型生成图片描述。
-    4. 替换 Markdown 中的图片链接为 MinIO URL。
-    """
+
 
     name: str = "node_md_img"
 
     def process(self, state: ImportGraphState) -> ImportGraphState:
+        """
+        节点: MD图片处理 (node_md_img)
+        1. 扫描 Markdown 中的图片链接。
+            PDF转的MD：扫描output/文档文件夹/images中的每一张支持格式的图片，逐个去转换完的md验证是否存在，存在才进行后面步骤
+            用户直接上传的MD：要求文件夹也是/images
+        2. 将图片上传到 MinIO 对象存储。
+        3. 调用多模态VLM模型生成图片描述。
+        4. 替换 Markdown 中的图片链接为 MinIO URL，并在[]中填充ai的描述信息
+        """
+
+
+
+
+
+
+
+
+
+
 
         return state
